bed3c2e5ce513564e50976ff0f42388a742e9e73
Kasun Kariyawasam <kasunsk@gmail.com>
2024-03-08 13:09:16 +0800

    Refactoring

--- src/main/java/com/hustlehub/dataservice/DataServiceApplication.java
+++ src/main/java/com/hustlehub/dataservice/DataServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mci.dataservice;
+package com.hustlehub.dataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hustlehub/dataservice/DataServiceApplicationTests.java
+++ src/test/java/com/hustlehub/dataservice/DataServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mci.dataservice;
+package com.hustlehub.dataservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
